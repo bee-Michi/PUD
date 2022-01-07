d42c92d78f31f31b73a3e36a4b71956a1a2a3e5d
bee_Michi <miczipersonal@gmail.com>
2022-01-07 11:11:40 +0100

    INITIAL COMMIT

---
+++ readme.md
@@ -0,0 +1,7 @@
+# Pub
+A beatiful python bot!
+
+This project is under heavy developement, please help me by contributing!
+
+## Liscence
+MIT Liscence
---
+++ src/main.py
@@ -0,0 +1,20 @@
+print("""
+------------------------------------------------------------PUB----------------------------------------------------------------------------
+""")
+version = "0.1.0"
+creator = "bee-Michi"
+contributors = {
+    "Currently nobady :( You can be the first one :)"
+}
+copytirgth = "Copytirgth© bee-Michi 2022"
+liscence = "This project is distributed under the MIT liscence. Learn more info at the liscence file :)"
+github = ""
+
+def help():
+    helpp = ["Help: This command!"]
+
+
+PUBseleect = input("Welcome! Use help to get all utilites and commands for some useful commands: ")
+
+if PUBseleect == "help":
+    print("Not yet implemented")
